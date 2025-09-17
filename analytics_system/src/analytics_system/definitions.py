21380f6402dc5979a621231927f4e7c3594ecee9
Michael Chapman <chapman.michael.c@gmail.com>
2025-09-17 15:47:22 -0500

    Getting first dlt ingestion pipeline working

--- analytics_system/src/analytics_system/definitions.py
+++ analytics_system/src/analytics_system/definitions.py
@@ -1,8 +1,23 @@
 from pathlib import Path
 
-from dagster import definitions, load_from_defs_folder
+from dagster import Definitions, definitions, load_from_defs_folder
+from dagster_dlt import DagsterDltResource
 
+from .defs.filesystem_duckdb_ingest.loads import calls_ingestion
+
+dlt_resource = DagsterDltResource()
 
 @definitions
 def defs():
-    return load_from_defs_folder(path_within_project=Path(__file__).parent)
+    default_defs = load_from_defs_folder(path_within_project=Path(__file__).parent)
+
+    dlt_defs = Definitions(
+        assets=[
+            calls_ingestion,
+        ],
+        resources={
+            "dlt": dlt_resource
+        }
+    )
+
+    return default_defs.merge(dlt_defs)
---
+++ analytics_system/src/analytics_system/defs/filesystem_duckdb_ingest/loads.py
@@ -0,0 +1,65 @@
+import os
+from typing import Optional
+
+import dlt
+import pathlib
+from dagster import AssetExecutionContext, DailyPartitionsDefinition, BackfillPolicy
+from dagster_dlt import DagsterDltResource, dlt_assets
+from dlt.sources.filesystem import filesystem, read_parquet
+
+daily_partitions = DailyPartitionsDefinition("2025-01-01", "2025-03-31")
+
+# Had to build absolute paths to route sources and destinations regardless of where this code is run
+# (ex: in dagster vs. locally)
+repo_dir = pathlib.Path(__file__).parent.parent.parent.parent.parent.resolve()
+warehouse_dir = repo_dir / ".data" / "warehouse"
+warehouse_dir.mkdir(parents=True, exist_ok=True)
+
+source_data_dir = pathlib.Path(__file__).parent.parent.parent.parent.parent.parent.resolve() / "data"
+
+@dlt.source
+def filesystem_calls_source(date_partition: Optional[str] | Optional[list[str]] = None):
+    @dlt.resource
+    def raw_calls():
+        if date_partition:
+            abs_path = os.path.abspath(os.path.join(source_data_dir, f"calls/day={date_partition}/")) # TODO: refactor this to be cleaner
+            partition_path = f"file://{abs_path}"
+
+            # Need to add the type ignore to the method chaining that dlt supports (piping into read_parquet())
+            yield from (filesystem(partition_path, file_glob="*.parquet") | read_parquet())  # type: ignore
+
+    return raw_calls
+
+
+@dlt_assets(
+    dlt_source=filesystem_calls_source(),
+    name="calls_ingestion_assets",
+    dlt_pipeline=dlt.pipeline(
+        pipeline_name="raw_calls_ingestion",
+        dataset_name="raw_calls",
+        destination=dlt.destinations.duckdb(os.path.abspath(os.path.join(warehouse_dir, "raw_calls_ingestion.duckdb")))
+    ),
+    partitions_def=daily_partitions,
+    backfill_policy=BackfillPolicy.multi_run(),
+    group_name="raw_ingestion",
+)
+def calls_ingestion(context: AssetExecutionContext, dlt: DagsterDltResource):
+    date = context.partition_key
+    yield from dlt.run(context=context, dlt_source=filesystem_calls_source(date_partition=date))
+
+
+# Testing the dlt code runs locally
+# Lesson learned: Running from dagster has a whole different starting location, so we needed to perform a more robust
+# absolute pathing structure. In a production deployment, this would likely be a remote location vs. a local one.
+
+# pipeline = dlt.pipeline(
+#         pipeline_name="raw_calls_ingestion",
+#         dataset_name="raw_calls",
+#         destination=dlt.destinations.duckdb(os.path.abspath(os.path.join(warehouse_dir, "raw_calls_ingestion.duckdb")))
+#     )
+#
+# load_info = pipeline.run(filesystem_calls_source(date_partition="2025-01-01"))
+# # with pipeline.sql_client() as client:
+# #     with client.execute_query("SELECT * FROM raw_calls;") as cursor:
+# #         print(cursor.fetchall())
+# print(load_info)
