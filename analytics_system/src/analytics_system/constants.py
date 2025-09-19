edc41f1c3ee8a6efc0c07275dc47f261f999d5f7
Michael Chapman <chapman.michael.c@gmail.com>
2025-09-19 11:46:39 -0500

    Updated location of warehouse directory to top level of project

--- analytics_system/src/analytics_system/constants.py
+++ analytics_system/src/analytics_system/constants.py
@@ -4,8 +4,8 @@ import pathlib
 # Had to build absolute paths to route sources and destinations regardless of where this code is run
 # (ex: in dagster vs. locally)
 
-repo_dir = pathlib.Path(__file__).parent.parent.parent.resolve()
-warehouse_dir = repo_dir / ".data" / "warehouse"
+repo_dir = pathlib.Path(__file__).parent.parent.parent.parent.resolve()
+warehouse_dir = repo_dir / "data" / "warehouse"
 warehouse_dir.mkdir(parents=True, exist_ok=True)
 
 WAREHOUSE_FILE_ABS_PATH = os.path.abspath(warehouse_dir / "data_warehouse.duckdb")
