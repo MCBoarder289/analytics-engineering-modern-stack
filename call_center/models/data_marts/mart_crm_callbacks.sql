5df8f351340f9df126ad4f5d204c43e263155c64
Michael Chapman <chapman.michael.c@gmail.com>
2026-01-11 15:57:21 -0600

    Adding mart_callbacks in order to compute first call resolution

---
+++ call_center/models/data_marts/mart_crm_callbacks.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = ['original_crm_id', 'callback_crm_id'],
+    incremental_strategy = 'delete+insert',
+    tags=['daily']
+    )
+}}
+
+select
+  crm1.crm_id as original_crm_id,
+  crm2.crm_id as callback_crm_id,
+  crm1.agent_id as original_agent_id,
+  crm2.agent_id as callback_agent_id,
+  crm1.customer_id,
+  crm1.created_ts as original_created_ts,
+  crm2.created_ts as callback_created_ts,
+  datediff('day', crm1.created_ts, crm2.created_ts) as days_between,
+  crm1.reason_code = crm2.reason_code as same_reason_flag,
+  crm1.sub_reason_code = crm2.sub_reason_code as same_sub_reason_flag,
+  crm2.previous_issue_flag as callback_previous_issue_flag,
+  NOW() as warehouse_updated_ts
+from {{ ref('mart_crm') }} crm1
+inner join {{ ref('mart_crm') }} crm2
+  on crm1.customer_id = crm2.customer_id
+  and crm2.created_ts > crm1.created_ts
+  and crm2.created_ts <= crm1.created_ts + interval '3 days'
+
+{% if is_incremental() %}
+where crm2.created_ts >= '{{ var("start_date") }}'
+  and crm2.created_ts < '{{ var("end_date") }}'
+{% endif %}
+
